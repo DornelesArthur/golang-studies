01285586b15f80f7b23a0313033894de363c1f4f
DornelesArthur <arthhdorneles@gmail.com>
2023-07-06 21:39:27 -0300

    How to create variables

---
+++ fundaments/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var weight int
+	weight = 10
+
+	var message string
+	message = "This is a message"
+
+	another_message := "This is an another message"
+
+	money := 100.25
+
+	fmt.Println(weight)
+	fmt.Println(message)
+	fmt.Println(another_message)
+	fmt.Println(money)
+}
