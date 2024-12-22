93a047a48ee58272d3388670c5809201b1adde23
DornelesArthur <arthhdorneles@gmail.com>
2024-12-21 22:44:48 -0300

    add: learn arrays in golang

---
+++ data_structures/1-arrays.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	string_array := [5]string{"test", "test", "test", "test", "test"}
+
+	fmt.Println(string_array)
+	fmt.Println(len(string_array))
+
+	int_array := [5]int{1, 3, 2, 5, 10}
+
+	fmt.Println(int_array)
+	fmt.Println(len(int_array))
+
+	var float_array [3]float64
+	pi := 3.1415
+
+	for i := 0; i < len(float_array); i++ {
+		float_array[i] = pi + float64(i)
+	}
+	fmt.Println(float_array)
+	fmt.Println(len(float_array))
+
+	bool_array := [3]bool{true, true, false}
+	for index, value := range bool_array {
+		fmt.Println(value)
+		fmt.Println(index)
+	}
+	fmt.Println(len(bool_array))
+
+}
