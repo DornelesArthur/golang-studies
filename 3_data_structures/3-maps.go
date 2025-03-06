a42161e0898a4f5b38ee900cdf894f7ed242af7e
DornelesArthur <arthhdorneles@gmail.com>
2025-03-05 21:44:11 -0300

    add: study maps

---
+++ 3_data_structures/3-maps.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// cities_population := map[string]int{"São Paulo": 10000000, "New York": 8000000, "Vancouver": 2642825}
+	cities_population := make(map[string]int)
+	cities_population["São Paulo"] = 10000000
+	cities_population["New York"] = 8000000
+	cities_population["Vancouver"] = 2642825
+	fmt.Println(cities_population)
+
+	if value, key_exists := cities_population["Paris"]; key_exists {
+		fmt.Println(value)
+	} else {
+		fmt.Println("Key dont exist")
+	}
+
+}
