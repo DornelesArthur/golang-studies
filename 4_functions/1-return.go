ec185ca9e4e9beb55c8cb00497de793600931fb3
DornelesArthur <arthhdorneles@gmail.com>
2025-03-11 21:45:09 -0300

    add: study functions:return named variables

--- 4_functions/1-return.go
+++ 4_functions/1-return.go
@@ -12,7 +12,19 @@ func returnMultipleValues(number1 int, number2 int) (int, int, int, float32) {
 
 	return sum, sub, multi, div
 }
+
+func returnNamedMultipleValues(number1 int, number2 int) (sum int, sub int, multi int, div float32) {
+	sum = number1 + number2
+	sub = number1 - number2
+	multi = number1 * number2
+	div = float32(number1) / float32(number2)
+
+	return
+}
 func main() {
 	sum, sub, multi, div := returnMultipleValues(1, 3)
+	fmt.Println(sum, sub, multi, div)
+
+	sum, sub, multi, div = returnNamedMultipleValues(4, 10)
 	fmt.Println(sum, sub, multi, div)
 }
