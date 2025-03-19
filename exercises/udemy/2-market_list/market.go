1d161b65d96fb996ec986f35c3c38970b2e7b0e1
DornelesArthur <arthhdorneles@gmail.com>
2025-03-18 21:27:40 -0300

    add: study how to validate and return errors

--- exercises/udemy/2-market_list/market.go
+++ exercises/udemy/2-market_list/market.go
@@ -7,17 +7,19 @@ import (
 )
 
 func main() {
-	marketlist := model.MarketList{
-		Created: time.Now(),
-		Date:    time.Now().AddDate(0, 0, 5),
-		Market:  "Generic SuperMarket",
-	}
+	itens := []model.Product{
+		{Name: "Condensed Milk", Value: 8.25, Quantity: 2, Unit: "395g"},
+		{Name: "Mayonnaise", Value: 19.99, Quantity: 1, Unit: "390g"},
+		{Name: "Chocolate Powder", Value: 12.99, Quantity: 1, Unit: "600g", Brand: "Nescau"},
+		{Name: "Bread", Value: 12.99, Quantity: 1, Unit: "400g"},
+		{Name: "Lager Beer", Value: 3.75, Quantity: 8, Unit: "350ml", Brand: "Brahma"}}
 
-	marketlist.Products = append(marketlist.Products, model.Product{Name: "Condensed Milk", Value: 8.25, Quantity: 2, Unit: "395g"})
-	marketlist.Products = append(marketlist.Products, model.Product{Name: "Mayonnaise", Value: 19.99, Quantity: 1, Unit: "390g"})
-	marketlist.Products = append(marketlist.Products, model.Product{Name: "Chocolate Powder", Value: 12.99, Quantity: 1, Unit: "600g", Brand: "Nescau"})
-	marketlist.Products = append(marketlist.Products, model.Product{Name: "Bread", Value: 12.99, Quantity: 1, Unit: "400g"})
-	marketlist.Products = append(marketlist.Products, model.Product{Name: "Lager Beer", Value: 3.75, Quantity: 8, Unit: "350ml", Brand: "Brahma"})
-	fmt.Println(marketlist)
+	marketlist, err := model.NewMarketList("Generic SuperMarket", time.Now().AddDate(0, 0, 5), itens)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(marketlist)
+	}
 
 }
--- exercises/udemy/2-market_list/model/market_list.go
+++ exercises/udemy/2-market_list/model/market_list.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"errors"
 	"time"
 )
 
@@ -9,4 +10,18 @@ type MarketList struct {
 	Created  time.Time
 	Date     time.Time
 	Market   string
+}
+
+func NewMarketList(market string, date time.Time, products []Product) (*MarketList, error) {
+
+	if market == "" {
+		return nil, errors.New("Market is mandatory")
+	}
+
+	return &MarketList{
+		Products: products,
+		Created:  time.Now(),
+		Date:     date,
+		Market:   market,
+	}, nil
 }
