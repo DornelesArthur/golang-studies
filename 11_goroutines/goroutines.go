edc360b2100c120d8d042b2f488901f62ff52ff4
DornelesArthur <arthhdorneles@gmail.com>
2025-03-24 22:51:26 -0300

    add: study sync.Mutex

--- 11_goroutines/goroutines.go
+++ 11_goroutines/goroutines.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
+	"time"
 )
 
 func main() {
 	var wait_group sync.WaitGroup
+
 	// goroutine is similar to threads (goroutines typically use 2kb of ram, instead of 2mb like threads)
 	// Go Scheduler decides (MN Scheduling) which goroutines will be in the same thread
 	for i := range 10 {
@@ -15,6 +17,24 @@ func main() {
 		// Just use go to call func async
 		go showMessage("Hello World! "+strconv.Itoa(i), &wait_group)
 		go showMessage2("Message2: "+strconv.Itoa(i), &wait_group)
+	}
+
+	var mutex sync.Mutex
+	number := 0
+
+	for x := 0; x < 10; x++ {
+		wait_group.Add(1)
+		go func() {
+			mutex.Lock()
+			number = x
+			time.Sleep(time.Second * 3)
+			// Without Mutex any gorountine can change the value of number at any time and will enter the if and exec panic()
+			if x != number {
+				panic("Number is the same as X")
+			}
+			mutex.Unlock()
+			wait_group.Done()
+		}()
 	}
 	wait_group.Wait()
 }
