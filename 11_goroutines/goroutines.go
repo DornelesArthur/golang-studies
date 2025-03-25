a4a772d30f1a62902ede8ed12b3b24347189d025
DornelesArthur <arthhdorneles@gmail.com>
2025-03-24 22:29:08 -0300

    add: study sync.WaitGroup

--- 11_goroutines/goroutines.go
+++ 11_goroutines/goroutines.go
@@ -3,20 +3,28 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"time"
+	"sync"
 )
 
 func main() {
+	var wait_group sync.WaitGroup
 	// goroutine is similar to threads (goroutines typically use 2kb of ram, instead of 2mb like threads)
 	// Go Scheduler decides (MN Scheduling) which goroutines will be in the same thread
-	for i := 0; i < 10; i++ {
+	for i := range 10 {
+		wait_group.Add(2)
 		// Just use go to call func async
-		go showMessage("Hello World! " + strconv.Itoa(i))
+		go showMessage("Hello World! "+strconv.Itoa(i), &wait_group)
+		go showMessage2("Message2: "+strconv.Itoa(i), &wait_group)
 	}
-
-	time.Sleep(time.Duration(time.Hour.Seconds() * float64(100)))
+	wait_group.Wait()
 }
 
-func showMessage(message string) {
+func showMessage(message string, wait_group *sync.WaitGroup) {
 	fmt.Println(message)
+	wait_group.Done()
+}
+
+func showMessage2(message string, wait_group *sync.WaitGroup) {
+	fmt.Println(message)
+	wait_group.Done()
 }
